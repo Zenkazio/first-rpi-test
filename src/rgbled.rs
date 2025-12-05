54c106a4e2aa2709e6a71c6e3c63503ecf21158b
zenkazio <zenkazio@gmail.com>
2025-12-05 21:40:22 +0100

    Create rgbled.rs

---
+++ src/rgbled.rs
@@ -0,0 +1,37 @@
+use rppal::gpio::{Error, Gpio, OutputPin};
+
+pub struct RGBLed {
+    r_pin: OutputPin,
+    g_pin: OutputPin,
+    b_pin: OutputPin,
+    freq: f64,
+}
+
+impl RGBLed {
+    pub fn new(r_pin: u8, g_pin: u8, b_pin: u8) -> Result<Self, Error> {
+        let r = Gpio::new()?.get(r_pin)?.into_output_low();
+        let g = Gpio::new()?.get(g_pin)?.into_output_low();
+        let b = Gpio::new()?.get(b_pin)?.into_output_low();
+        Ok(RGBLed {
+            r_pin: r,
+            g_pin: g,
+            b_pin: b,
+            freq: 1000.0,
+        })
+    }
+    pub fn set_rgb(&mut self, r: u8, g: u8, b: u8) -> Result<(), Error> {
+        self.r_pin
+            .set_pwm_frequency(self.freq, r as f64 / 255 as f64)?;
+        self.g_pin
+            .set_pwm_frequency(self.freq, g as f64 / 255 as f64)?;
+        self.b_pin
+            .set_pwm_frequency(self.freq, b as f64 / 255 as f64)?;
+        Ok(())
+    }
+    pub fn clear(&mut self) -> Result<(), Error> {
+        self.r_pin.clear_pwm()?;
+        self.g_pin.clear_pwm()?;
+        self.b_pin.clear_pwm()?;
+        Ok(())
+    }
+}
