989952591b80295c3fb11d0ac024038be0abe701
zenkazio <zenkazio@gmail.com>
2025-12-11 21:27:59 +0100

    Update pins.rs

--- src/pins.rs
+++ src/pins.rs
@@ -1,8 +1,9 @@
 #![allow(dead_code)]
 pub const RED_LED: u8 = 23; // 16
-pub const INPUT_BUTTON: u8 = 18; // 12
+pub const INPUT_BUTTON: u8 = 26; // 12
 pub const RGB_LED_RED: u8 = 17; //11
 pub const RGB_LED_GREEN: u8 = 27; //13
 pub const RGB_LED_BLUE: u8 = 22; //15
-pub const TRIG: u8 = 14; // 8
-pub const ECHO: u8 = 15; // 10
+pub const TRIG: u8 = 6; // 8
+pub const ECHO: u8 = 16; // 10
+pub const SERVO_PULS: u8 = 13; // 37
