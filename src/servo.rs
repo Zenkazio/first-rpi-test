2b71247ccb2cf3dcb15cbc8a5e5f6cfcf3b2fc9d
zenkazio <zenkazio@gmail.com>
2025-12-14 18:31:37 +0100

    changed servo def

--- src/servo.rs
+++ src/servo.rs
@@ -5,7 +5,7 @@ use rppal::gpio::{Error, Gpio, OutputPin};
 
 pub struct Servo {
     pulse_pin: OutputPin,
-    last_degree: u8,
+    last_degree: i8,
 }
 
 impl Servo {
@@ -17,15 +17,19 @@ impl Servo {
         Ok(s)
     }
 
-    pub fn set_degree(&mut self, degree: u8) {
-        if degree > 180 {
+    pub fn set_degree(&mut self, degree: i8) {
+        if degree > 90 || degree < -90 {
             return;
         }
         // always a 50Hz base
         self.pulse_pin
-            .set_pwm_frequency(50.0, 0.05 + (degree as f64 / 180.0) * 0.05 * 1.6);
+            .set_pwm_frequency(50.0, (degree as f64 * (0.025 / 90.0)) + 0.075);
         let diff = self.last_degree.abs_diff(degree);
         thread::sleep(Duration::from_millis(2 * diff as u64));
         self.last_degree = degree;
     }
 }
+
+// 0 -> 1.5ms -> 0.075
+// -90 -> 1ms -> 0.05
+// 90 -> 2ms -> 0.1
